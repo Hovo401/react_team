b95a9b932d983ff5d26eaab15d2940a142031a27
Diotek <hovo.55.k.yan@gmail.com>
2022-07-21 06:20:51 +0400

    start-main

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Es mer main branch-y 🎉</Text>
+      <br/>
+      <Text>{"sran chkpneq )"}</Text>
       <StatusBar style="auto" />
     </View>
   );
