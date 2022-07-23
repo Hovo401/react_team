0d2d716f7c5d06c62d710acad5419c400a18809a
Diotek <hovo.55.k.yan@gmail.com>
2022-07-24 00:04:45 +0400

    kodi krjadum kompakt versiya

--- App.js
+++ App.js
@@ -1,41 +1,19 @@
 import React from 'react';
 import { SafeAreaView, View, VirtualizedList, StyleSheet, Text, StatusBar } from 'react-native';
 import { useState, useEffect } from 'react';
-// your components
+import { Render } from './src/Components/Render/Rendering.js'
 import { Navbar } from './src/Components/Navbar';
-import { Home } from './src/Components/Home';
-import { Search } from './src/Components/Search';
-import { Music } from './src/Components/Music';
-import { Account } from './src/Components/Account';
 
-//========= Render Stranic =========
-const Rendr_stranic={
-Home :()=> { return <Home key={'home'}/> },
-Search :()=> { return <Search key={'edewew'}/> },
-Music :()=> { return <Music key={'hoewfewfme'}/> },
-Account :()=> { return <Account key={'we'}/> },
-}
- const Render = (Name_stranic)=>{
-   for(var data in Rendr_stranic){
-     if (data==Name_stranic){
-       return Rendr_stranic[data]()
-     }
-   }
- }
-//==================================
 
 const App = () => {
-
-
   const [renderName, setRenderName] = useState("Home");
-
 
   return (
     <>
       <View style={styles.container}>
-     { Render(renderName)}
+        {Render(renderName)}
       </View>
-      
+
       <Navbar setRenderName={setRenderName} />
     </>
   );
