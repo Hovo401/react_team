4fe0beb784826c243e8473e4e14d054e7b21446b
MaryGasparyan <meri.g1.y@tumo.org>
2022-07-27 15:19:34 +0400

    Account page elements flex

--- src/Components/Account.js
+++ src/Components/Account.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {SafeAreaView, View, Text, StyleSheet, Image, Pressable } from 'react-native';
+import { SafeAreaView, View, Text, StyleSheet, Image, Pressable } from 'react-native';
 
 //import { SafeAreaView } from 'react-native-safe-area-context';
 //import * as Font from 'expo-font';
 
-export const Account = ({setRenderName}) => {
+export const Account = ({ setRenderName }) => {
     // this.state={
     //     fontLoaded: false
     //  }
@@ -27,47 +27,47 @@ export const Account = ({setRenderName}) => {
 
             </View>
 
-            <View style={{flex:4,backgroundColor: "white",  width:'100%',alignItems: 'center',}}>
-            <View style={styles.thirdDiv}>
-            <View style={styles.alignHorizontal}>
-            <Image style={styles.image1} source={require("../Icons/edit.png")}></Image>
-            <Image style={styles.logo} source={require("../Icons/person.png")}></Image>
-            <Pressable onPress={() => {
-                console.log(setRenderName);
-                
-            setRenderName("Settings");
-          
-          }}><Image style={styles.image1} source={require("../Icons/settings.png")} ></Image></Pressable>
-            
-            </View>
-                
-                <Text>Username</Text>
-                <Text>e-mail@gmail.com</Text>
-                <View style={{alignSelf: "flex-start", margin:10}}>
-                <Pressable style={styles.button}>
-                
-                    <Image style={styles.image} source={require("../Icons/folder.png")}></Image>
-                    <Text style={{width:"74%",}}>My songs</Text>
-                    <Image style={[styles.next, styles.image]} source={require("../Icons/nextArrow.png")}></Image>
-                </Pressable>
-                <Pressable style={styles.button}>
-                    <Image style={styles.image} source={require("../Icons/share.png")}></Image>
-                    <Text style={{width:"74%",}}>Share with friends</Text>
-                    <Image style={[styles.next, styles.image]} source={require("../Icons/nextArrow.png")}></Image>
-                </Pressable>
-                <Pressable style={styles.button}>
-                    <Image style={styles.image} source={require("../Icons/folder.png")}></Image>
-                    <Text style={{width:"74%",}}>Help</Text>
-                    <Image style={[styles.next, styles.image]} source={require("../Icons/nextArrow.png")}></Image>
-                </Pressable>
-                </View>
-               
-            </View>
-            <View style = {{flex:1}}></View>
+            <View style={{ flex: 4, backgroundColor: "white", width: '100%' , alignItems: 'center', }}>
+                <View style={styles.thirdDiv}>
+                    <View style={styles.alignHorizontal}>
+                        <Image style={styles.image1} source={require("../Icons/edit.png")}></Image>
+                        <Image style={styles.logo} source={require("../Icons/person.png")}></Image>
+                        <Pressable onPress={() => {
+                            console.log(setRenderName);
 
-                    
+                            setRenderName("Settings");
+
+                        }}><Image style={styles.image1} source={require("../Icons/settings.png")} ></Image></Pressable>
+
                     </View>
-            
+
+                    <Text>Username</Text>
+                    <Text>e-mail@gmail.com</Text>
+                    <View style={{ width:"100%"}}>
+                        <Pressable style={styles.button}>
+
+                            <Image style={styles.image} source={require("../Icons/folder.png")}></Image>
+                            <Text style={{flex: 7}}>My songs</Text>
+                            <Image style={[styles.next, styles.image]} source={require("../Icons/nextArrow.png")}></Image>
+                        </Pressable>
+                        <Pressable style={styles.button}>
+                            <Image style={styles.image} source={require("../Icons/share.png")}></Image>
+                            <Text style={{flex: 7}}>Share with friends</Text>
+                            <Image style={[styles.next, styles.image]} source={require("../Icons/nextArrow.png")}></Image>
+                        </Pressable>
+                        <Pressable style={styles.button}>
+                            <Image style={styles.image} source={require("../Icons/folder.png")}></Image>
+                            <Text style={{flex: 7}}>Help</Text>
+                            <Image style={[styles.next, styles.image]} source={require("../Icons/nextArrow.png")}></Image>
+                        </Pressable>
+                    </View>
+
+                </View>
+                <View style={{ flex: 1 }}></View>
+
+
+            </View>
+
 
 
         </View>
@@ -75,17 +75,17 @@ export const Account = ({setRenderName}) => {
     )
 }
 const styles = StyleSheet.create({
-    button:{
+    button: {
         alignItems: "flex-end",
         flexDirection: 'row',
         marginBottom: 6,
     },
-    alignHorizontal:{
+    alignHorizontal: {
         alignItems: "flex-end",
         flexDirection: 'row',
         justifyContent: 'space-between',
-         
-        width:'100%'
+
+        width: '100%'
     },
     container: {
         flex: 1,
@@ -100,44 +100,46 @@ const styles = StyleSheet.create({
     //     fontFamily: 'OpenSans-Bold'
     // },
     image1: {
-        
+
         height: 30,
         width: 30,
-        marginHorizontal:"5%",
+ 
     },
     image: {
-        
+
         height: 30,
         width: 30,
-        marginRight:"5%",
+        marginRight: "5%",
+        
+       
     },
     firstDiv: {
         justifyContent: 'center',
-        padding:10,
+        padding: 10,
         width: "100%",
         flex: 1,
         backgroundColor: "black",
 
     },
-   
+
     logo: {
         //justifyContent: 'center', 
-        marginVertical:5,
+        marginVertical: 5,
         height: 100,
         width: 100,
         borderRadius: 50,
-        
+
     },
-   
+
     thirdDiv: {
         alignItems: 'center',
         backgroundColor: "#D2CFD6",
-        
+        padding:10,
         flex: 4,
         width: "80%",
 
     },
-    next:{
-        width:"20%",
+    next: {
+        
     }
 });
