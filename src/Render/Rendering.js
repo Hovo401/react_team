65bf5484dc334f3943e9fcd2ee5f17b89907b5e6
user <anahitbaghramyan13@gmail.com>
2022-07-27 15:14:29 +0400

    Album Design

--- src/Render/Rendering.js
+++ src/Render/Rendering.js
@@ -4,15 +4,17 @@ import { Search } from '../Components/Search';
 import { Music } from '../Components/Music';
 import { Account } from '../Components/Account';
 import { Settings } from '../Components/Settings';
+import {Music_list} from "../Components/Music_list"
 
 export const Render = ({renderName,setRenderName}) => {
 
     const Rendr_stranic = {
         Home: () => { return <Home key={'home'} /> },
         Search: () => { return <Search key={'edewew'} /> },
-        Music: () => { return <Music key={'hoewfewfme'} /> },
+        Music: () => { return <Music setRenderName={setRenderName} key={'hoewfewfme'} /> },
         Account: () => { return <Account setRenderName={setRenderName} key={'we'} /> },
         Settings: () => { return <Settings setRenderName={setRenderName} key={'tfgg'} /> },
+        Music_list:()=>{return <Music_list  key={'trhgergre'}></Music_list>}
     }
 console.log(setRenderName);
 
