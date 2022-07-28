f5e60c3156ff780a6d1966a0e2cc13bc063116a4
Diotek <hovo.55.k.yan@gmail.com>
2022-07-28 10:01:56 +0400

    error RNSound.IsAndroid

--- App.js
+++ App.js
@@ -3,38 +3,12 @@ import { SafeAreaView, View, VirtualizedList, StyleSheet, Text, StatusBar } from
 import { useState, useEffect } from 'react';
 import { Render } from './src/Render/Rendering.js'
 import { Navbar } from './src/Components/Navbar';
-import {Audio} from "expo-av"
+
 
 const App = () => {
   const [renderName, setRenderName] = useState("Home");
   //=================== music player===================
 
-  const [recording, setRecording]= useState();
-  async function startRecordin(){
-    try{
-      console.log('miacav');
-      await Audio.requestPermissionsAsync();
-      await Audio.setAudioModeAsync({
-        allowsRecordingIOS:true,
-        playsInSilentModeIOS:true,
-      });
-      console.log('start music...');
-      const recording = new Audio.Recording();
-      await recording.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
-      await recording.startAsync();
-      setRecording(recording);
-      console.log('serRecording');
-    }catch(err){
-      console.error("err ", err)
-    }
-  }
-  async function stopRecording(){
-    console.log("stoping");
-    setRecording(undefined);
-    await recording.stopAndunloadAsync();
-    const url = recording.getURL();
-    console.log('stop tr');
-  }
 
   //===================================================
   return (
--- src/Components/Music_list.js
+++ src/Components/Music_list.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import { SafeAreaView, View, VirtualizedList, StyleSheet, Pressable, Text, StatusBar, Image } from 'react-native';
-
-
+import Audio from "../Render/Audio"
 
 export const Music_list = () => {
 
     return (
-        <View >
-
-            
-                    <Text >Your playlists:</Text>
-             
-
-
-        </View>
+        <Audio></Audio>
     )
 }
